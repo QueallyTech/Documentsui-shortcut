09f07e2e354508e401d360254f68ed2cd858dbd4
queally.tech <>
2023-04-29 23:41:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Documentsui-shortcut
+
+Create shotcut to open system's native file exporer  
+using `content://com.android.externalstorage.documents/root/primary` with `android.intent.action.VIEW`
+
+support package name:  
+
+- com.android.documentsui
+- com.google.android.documentsui
---
+++ app/src/main/java/com/documentsui/shortcut/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.documentsui.shortcut
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val tag = "DocumentsUI-Shortcut"
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent()
+        intent.action = "android.intent.action.VIEW"
+        intent.data = Uri.parse("content://com.android.externalstorage.documents/root/primary")
+        try {
+            intent.setClassName(
+                "com.google.android.documentsui",
+                "com.android.documentsui.files.FilesActivity"
+            )
+            Log.d(tag,"try to start com.google.android.documentsui")
+            startActivity(intent)
+        } catch (e: Exception) {
+            Log.i(tag,"fail to start com.google.android.documentsui")
+            try {
+                intent.setClassName(
+                    "com.android.documentsui",
+                    "com.android.documentsui.files.FilesActivity"
+                )
+                Log.d(tag,"try to start com.android.documentsui")
+                startActivity(intent)
+            } catch (e1: Exception) {
+                Log.i(tag,"fail to start com.android.documentsui")
+                e.addSuppressed(e1);
+                Log.e(tag, e.toString())
+            }
+        }
+        finish()
+    }
+}
